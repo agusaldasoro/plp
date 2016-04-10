b6473107350c5b50dbaaf21ae462520ff7ecf5b1
Nico <nico@NicoChamos-MacBook-Pro-4.local>
2016-04-10 13:06:33 -0300

    First commit

--- TP1/Tests-alu.hs
+++ TP1/Tests-alu.hs
@@ -5,19 +5,45 @@ import Solucion
 import Test.HUnit
 import Data.List
 
+mucho = 1000000
+
 -- evaluar main para correr todos los tests
 main = runTestTT allTests
 
 allTests = test [
  	"split" ~: testsSplit,
- 	"cuentas" ~: testsCuentas
-]
+  "longPromedioPalabras" ~: testsLongitudPromedioPalabras,
+ 	"cuentas" ~: testsCuentas,
+  "frecuenciaTokens" ~: testsFrecuenciaTokens
+  ]
 
 testsSplit = test [
  	split ',' ",PLP," ~?= ["PLP"],
  	split ',' " ,PLP, " ~?= [" ","PLP"," "]
-]
+  ]
+
+testsLongitudPromedioPalabras = test [
+  longitudPromedioPalabras "Este test tiene palabras $$++$$" ~?= 5.4,
+  longitudPromedioPalabras "1" ~?= 1,
+  longitudPromedioPalabras "Hola a todo" ~?= 3,
+  longitudPromedioPalabras "3 tristes tigres" ~?= 4.6666665,
+  longitudPromedioPalabras "Mi mama me mima" ~?= 3,
+  longitudPromedioPalabras [if x `mod` 2 == 0 then ' ' else 'a' | x <- [1 .. mucho]] ~?= 1
+  ]
 
 testsCuentas = test [
 	cuentas ["x","x","y","x","z"] ~?= [(3,"x"), (1,"y"), (1,"z")]
-]
+  ]
+
+testsFrecuenciaTokens = test [
+  buscarExtractor '_' "use_snake_case !" ~?= 0.125,
+  buscarExtractor '!' "use_snake_case !" ~?= 0.0625,
+  buscarExtractor 's' "use_snake_case !" ~?= 0.1875,
+  buscarExtractor 'h' "use_snake_case !" ~?= 0.0,
+  buscarExtractor 'h' "" ~?= 0.0,
+  buscarExtractor 'a' ['a' | x <- [1 .. mucho]] ~?= 1
+  ]
+
+buscarExtractor = \tok ->
+        let posToken = (\(Just i) -> i) (elemIndex tok tokens)
+        in frecuenciaTokens !! posToken
